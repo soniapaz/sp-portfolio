cc95f0d7bf4d31d887b75695423b94ccd1a17005
soniapaz <pazaguilarsonia@gmail.com>
2023-07-26 21:14:11 +0200

    Change homepage structure, improve responsiveness and add transitions

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,10 @@
+// Navbar scroll
 const header = document.querySelector("header");
 let prevScrollPos = window.scrollY;
 let isHeaderAtTop = true;
 const originalPaddingTop = "3rem";
 
-function handleScroll() {
+function handleNavbarScroll() {
   const currentScrollPos = window.scrollY;
 
   if (prevScrollPos > currentScrollPos) {
@@ -28,4 +29,36 @@ function handleScroll() {
   prevScrollPos = currentScrollPos;
 }
 
-window.addEventListener("scroll", handleScroll);
+window.addEventListener("scroll", handleNavbarScroll);
+
+// Section scroll
+let hasFadeIn = false;
+
+// Check if the element is visible in the viewport
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+
+// Handle the scroll event
+function handleSectionScroll() {
+  const featuredProjectsSection = document.getElementById("featured-projects");
+  const featuredProjectsTitle = document.querySelector("#featured-projects h1");
+
+  // Check if the H1 is in the viewport and the transition has not been applied yet
+  if (!hasFadeIn && isElementInViewport(featuredProjectsTitle)) {
+    // Apply the "fade-in-up" class to the entire section
+    featuredProjectsSection.classList.add("fade-in-up");
+    // Update the flag to avoid repetitions
+    hasFadeIn = true;
+  }
+}
+
+// Add the DOMContentLoaded event to ensure the document is fully loaded
+window.addEventListener("DOMContentLoaded", function () {
+  // Add the scroll event once the DOM is fully loaded
+  window.addEventListener("scroll", handleSectionScroll);
+});
